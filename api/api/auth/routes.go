9ed67579e4cc9336a70dd3fc34aea6f654869483
Joseph Azevedo <joseph.az@gatech.edu>
2020-10-30 04:11:50 -0400

    fix: use actual 302 status code

--- api/api/auth/routes.go
+++ api/api/auth/routes.go
@@ -213,6 +213,6 @@ func terminalRedirect(w http.ResponseWriter, r *http.Request,
 	q.Add(key, value)
 	u.RawQuery = q.Encode()
 
-	http.Redirect(w, r, u.String(), http.StatusTemporaryRedirect)
+	http.Redirect(w, r, u.String(), http.StatusMovedTemporarily)
 	return nil
 }
--- api/cas/provider.go
+++ api/cas/provider.go
@@ -51,7 +51,7 @@ func (c *Provider) Redirect(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	http.Redirect(w, r, redirectUrl, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, redirectUrl, http.StatusMovedTemporarily)
 	return nil
 }
 
