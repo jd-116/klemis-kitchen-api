42c090b693411daaf18434dddcddfe6c64c3ec37
Bhanu Garg <garg.bhanu1@gmail.com>
2020-10-30 14:49:41 -0700

    chore: add and update product patch route

--- api/api/products/routes.go
+++ api/api/products/routes.go
@@ -22,6 +22,7 @@ func Routes(database db.Provider, products products.Provider) *chi.Mux {
 	router := chi.NewRouter()
 	router.Get("/", GetAll(database, database, products))
 	router.Get("/{id}", GetSingle(database, database, products))
+	router.Patch("/{id}", Update(database))
 	return router
 }
 
@@ -213,6 +214,42 @@ func GetSingle(productMetadataProvider db.ProductMetadataProvider, locationProvi
 
 		// Return the single product as the top-level JSON
 		jsonResponse, err := json.Marshal(resultProduct)
+		if err != nil {
+			util.ErrorWithCode(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
+	}
+}
+
+// Update updates a products metadata in the database
+func Update(productMetadataProvider db.ProductMetadataProvider) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		if id == "" {
+			util.ErrorWithCode(w, errors.New("the URL parameter is empty"),
+				http.StatusBadRequest)
+			return
+		}
+
+		partial := make(map[string]interface{})
+		err := json.NewDecoder(r.Body).Decode(&partial)
+		if err != nil {
+			util.Error(w, err)
+			return
+		}
+
+		updated, err := productMetadataProvider.UpdateProduct(r.Context(), id, partial)
+		if err != nil {
+			util.Error(w, err)
+			return
+		}
+
+		// Return the updated product metadata as the top-level JSON
+		jsonResponse, err := json.Marshal(updated)
 		if err != nil {
 			util.ErrorWithCode(w, err, http.StatusInternalServerError)
 			return
