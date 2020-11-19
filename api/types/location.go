fee6d2e830a7b193ff978e36b122355bd39ee9a4
Joseph Azevedo <joseph.az@gatech.edu>
2020-11-19 01:07:39 -0500

    feat: add automatic ID provisioning

--- api/types/location.go
+++ api/types/location.go
@@ -5,26 +5,26 @@ package types
 // Note: this struct should not be returned from the API directly;
 // instead, use the LocationMetadata struct
 type Location struct {
-	TransactIdentifier string         `json:"transact_identifier" bson:"transact_identifier"`
 	ID                 string         `json:"id" bson:"id"`
 	Name               string         `json:"name" bson:"name"`
 	Location           GeoCoordinates `json:"location" bson:"location"`
+	TransactIdentifier string         `json:"transact_identifier" bson:"transact_identifier"`
 }
 
 // Inner gets the inner representation for this location
 func (l *Location) Inner() LocationMetadata {
 	return LocationMetadata{
-		ID:           l.ID,
-		Name:         l.Name,
-		Location:     l.Location,
+		ID:       l.ID,
+		Name:     l.Name,
+		Location: l.Location,
 	}
 }
 
 // LocationMetadata is the external representation of a location
 type LocationMetadata struct {
-	ID           string         `json:"id"`
-	Name         string         `json:"name"`
-	Location     GeoCoordinates `json:"location"`
+	ID       string         `json:"id"`
+	Name     string         `json:"name"`
+	Location GeoCoordinates `json:"location"`
 }
 
 // GeoCoordinates is the representation of a pair of GPS coordinates
@@ -32,4 +32,11 @@ type LocationMetadata struct {
 type GeoCoordinates struct {
 	Latitude  float64 `json:"latitude" bson:"latitude"`
 	Longitude float64 `json:"longitude" bson:"longitude"`
+}
+
+// LocationCreate is the partial Location struct that is sent in POST requests
+type LocationCreate struct {
+	Name               string         `json:"name" bson:"name"`
+	Location           GeoCoordinates `json:"location" bson:"location"`
+	TransactIdentifier string         `json:"transact_identifier" bson:"transact_identifier"`
 }
